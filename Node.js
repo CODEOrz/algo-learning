0c1a217f2f54bb57218b00dc3bfc2cf5e6f331a1
CODEOrz <406576774@qq.com>
2020-03-20 12:51:22 +0800

    add Tree and Node Class

---
+++ Node.js
@@ -0,0 +1,13 @@
+export default class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data
+    this.left = left
+    this.right = right
+    this.count = 1
+  }
+
+  show() {
+    return this.data
+  }
+
+}
