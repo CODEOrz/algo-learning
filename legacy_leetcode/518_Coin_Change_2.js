dc0a8293684da7fda7bac13cff31393e0d57c52d
CODEOrz <406576774@qq.com>
2020-05-21 17:04:42 +0800

    update #518 to 'leetcode'

---
+++ legacy_leetcode/518_Coin_Change_2.js
@@ -0,0 +1,55 @@
+// 解法一
+var change = function (amount, coins) {
+  let dp = new Array(amount + 1).fill(0)
+  dp[0] = 1
+
+  for (let coin of coins) {
+    for (let j = 1; j <= amount; j++) {
+      if (j >= coin) {
+        dp[j] += dp[j - coin]
+        // console.log(`coin = ${coin}, j = ${j}, dp${j} = ${JSON.stringify(dp[j])}, dp = ${JSON.stringify(dp)}`) 
+      }
+    }
+  }
+
+  return dp[amount]
+}
+
+export default change
+
+/*****************************************************************************************************************************************************
+[Question]
+You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+Example 1:
+
+Input: amount = 5, coins = [1, 2, 5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+Example 2:
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+Example 3:
+
+Input: amount = 10, coins = [10] 
+Output: 1
+ 
+
+Note:
+
+You can assume that
+
+0 <= amount <= 5000
+1 <= coin <= 5000
+the number of coins is less than 500
+the answer is guaranteed to fit into signed 32-bit integer
+
+[Comment]
+
+********************************************************************************************************************************************************/
