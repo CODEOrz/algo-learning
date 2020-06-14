4bf9de2993eb166025244633e3e5e123a9c75d36
CODEOrz <406576774@qq.com>
2020-06-14 21:44:49 +0800

    add a case to 'legacy_leetcode'

---
+++ legacy_leetcode/37_Sudoku_Solver(解数独).js
@@ -0,0 +1,88 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+
+
+/**
+ * 解法一 : 自己想的
+ */
+
+var solveSudoku = function (board) {
+  const numsSet = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  const boardLen = board.length
+  const dimension = 3
+
+  function findAvailableNums(row, col, board) {
+    let existedSet = new Set()
+
+    /* find all existed nums in the same row and the same col  */
+    board[row].forEach((v) => existedSet.add(v))
+    for (let i = 0; i < boardLen; ++i) {
+      existedSet.add(board[i][col])
+    }
+
+    /* find all existed nums in current sub-box  */
+    const currentRowBlock = Math.floor(row / dimension)
+    const currentColBlock = Math.floor(col / dimension)
+    for (let i = currentRowBlock * dimension; i < (currentRowBlock + 1)* dimension; ++i) {
+      for (let j = currentColBlock * dimension; j < (currentColBlock+1) * dimension; ++j) {
+        existedSet.add(board[i][j])
+      }
+    }
+
+    return numsSet.filter((v) => !existedSet.has(v))
+  }
+
+  function solver(index, board) {
+    if (index === boardLen * boardLen) return true
+
+    const row = Math.floor(index / boardLen)
+    const col = index % boardLen
+
+    if (board[row][col] === '.') {
+      const availableNums = findAvailableNums(row, col, board)
+      if (availableNums.length > 0) {
+        for (let k = 0; k < availableNums.length; ++k) {
+          board[row][col] = availableNums[k]
+          if (solver(index + 1, board)) return true
+          board[row][col] = '.'
+        }
+      }
+    } else {
+      return solver(index + 1, board)
+    }
+  }
+
+  solver(0, board)
+}
+
+export default solveSudoku
+
+/*****************************************************************************************************************************************************
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+Empty cells are indicated by the character '.'.
+
+
+A sudoku puzzle...
+
+
+...and its solution numbers marked in red.
+
+Note:
+
+The given board contain only digits 1-9 and the character '.'.
+You may assume that the given Sudoku puzzle will have a single unique solution.
+The given board size is always 9x9.
+
+********************************************************************************************************************************************************
+[Comment]
+
+
+********************************************************************************************************************************************************/
--- legacy_leetcode/77_Combinations(组合).js
+++ legacy_leetcode/77_Combinations(组合).js
@@ -6,32 +6,62 @@
 
 /**
  * 解法一，自己想的
+ * 124 ms, faster than 57.14% 
+ * 124 ms, faster than 57.14% 
+ */
+
+// var combine = function (n, k) {
+//   let arr = []
+//   for (let i = 0; i < n; ++i) {
+//     arr.push(i + 1)
+//   }
+
+//   function combination(arr, num) {
+//     if (1 === num--) {
+//       return arr.map((i) => [i])
+//     }
+//     let res = []
+
+//     while (arr.length >= num) {
+//       const curr = arr.pop()
+//       let result = combination([...arr], num)
+//       for (let j = 0; j < result.length; ++j) {
+//         result[j].push(curr)
+//       }
+//       res = res.concat(result)
+//     }
+//     return res
+//   }
+
+//   return combination(arr, k)
+// }
+
+/**
+ * 解法二，DFS解法
+ * 228 ms, faster than 18.61%
+ * 45.2 MB, less than 12.23% 
  */
 
 var combine = function (n, k) {
-  let arr = []
-  for (let i = 0; i < n; ++i) {
-    arr.push(i + 1)
+  let res = []
+  if (k <= 0 || n <= 0) return res
+
+  function backtrack(n, k, start, track) {
+    if (k === track.length) {
+      res.push(track)
+      return
+    }
+
+    for (let i = start; i <= n; ++i) {
+      track.push(i)
+      backtrack(n, k, i + 1, [...track])
+      track.pop()
+    }
   }
 
-  function combination(arr, num) {
-    if (1 === num--) {
-      return arr.map((i) => [i])
-    }
-    let res = []
-
-    while (arr.length >= num) {
-      const curr = arr.pop()
-      let result = combination([...arr], num)
-      for (let j = 0; j < result.length; ++j) {
-        result[j].push(curr)
-      }
-      res = res.concat(result)
-    }
-    return res
-  }
-
-  return combination(arr, k)
+  let track = []
+  backtrack(n, k, 1, [...track])
+  return res
 }
 
 export default combine
