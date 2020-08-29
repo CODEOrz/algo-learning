83152ca158c95427edb27da185a0cd06a0e446e7
CODEOrz <406576774@qq.com>
2020-08-29 20:45:57 +0800

    add case to 'legacy_leetcode'

---
+++ legacy_leetcode/24.两两交换链表中的节点.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (head === null || head.next === null) {
+    return head
+  }
+  let newHead = head.next
+  const temp = head.next.next
+  newHead.next = head
+  head.next = swapPairs(temp)
+  return newHead
+}
+// @lc code=end
+
